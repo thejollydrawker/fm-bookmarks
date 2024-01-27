33e68d9ee7d7f087a549415fd0688db07c3350fc
thejollydrawker <rekaketezeregy@gmail.com>
2024-01-27 21:12:32 +0100

    adding card component

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,6 +1,5 @@
 import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
-import {styleMap} from 'lit/directives/style-map.js';
 
 
 @customElement('bookmark-btn')
@@ -21,13 +20,18 @@ class BookmarkBtn extends LitElement {
 
     static styles = css`
         .bookmark-btn {
-            padding: 0.5rem 1rem;
+            box-sizing: border-box;
+            padding: 0.8rem 1rem;
             color: white;
             border: none;
             border-radius: 3px;
             font-family: inherit;
+            font-size: 14px;
+            font-weight: 500;
+            width: 100%;
             cursor: pointer;
             background-color: var( --btn-color, 'grey' );
+            border: solid 1px var( --btn-color, 'grey' );
         }
 
         .bookmark-btn:hover {
---
+++ src/components/card.ts
@@ -0,0 +1,72 @@
+import { LitElement, html, css } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+import './button';
+
+import { globalStyles } from "../styles";
+
+@customElement('bookmark-card')
+class BookmarkCard extends LitElement {
+
+    @property({type: Object})
+    content?: { header: String, subheader: String };
+
+    render() {
+        return html`
+            <article>
+                <slot></slot>
+                <h2>${this.content.header}</h2>
+                <h3>${this.content.subheader}</h3>
+                <div class="divider"></div>
+                <bookmark-btn class="blue" content="Add & Install Extension"></bookmark-btn>
+            </article>
+        `;
+    }
+
+    static styles = [
+        globalStyles,
+        css`
+        bookmark-btn.blue {
+            --btn-color: var(--soft-blue);
+            margin-top: 1rem;
+            width: 75%;
+        }
+
+        article {
+            width: 100%;
+            padding: 2rem 0rem;
+            overflow-x: hidden;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            border-radius: 8px;
+            --shadow-color: 0px 5px 5px lightgray, -5px 5px 5px lightgray, 5px 5px 5px lightgray;
+            box-shadow: var(--shadow-color);
+        }
+
+        h2 {
+            font-size: 18px;
+            font-weight: 500;
+            width: fit-content;
+            margin-top: 2rem;
+            color: var(--dark-blue);
+        }
+
+        h3 {
+            font-size: 14px;
+            font-weight: 400;
+            width: fit-content;
+            margin: 0;
+            margin-bottom: 0.5rem;
+            color: var(--grayish-blue);
+        }
+
+        .divider::before {
+            content: '.......................................................';
+            font-size: 32px;
+            opacity: 0.5;
+            letter-spacing: 10px;
+            color: var(--grayish-blue);
+        }
+    `];
+}
